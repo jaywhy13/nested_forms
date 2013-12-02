8732c0b5e7f2b808216111d6a78f967e20f58b23
Jean-Mark Wright <jeanmark.wright@gmail.com>
2013-12-02 12:43:04 -0500

    Added Django extensions

--- nested_forms/settings.py
+++ nested_forms/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'django_extensions',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
