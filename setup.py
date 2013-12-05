7e9f00281e83953c447fa6be489020ce28e8e8bc
Jean-Mark Wright <jeanmark.wright@gmail.com>
2013-12-04 20:39:45 -0500

    Added a setup.py so this can be installed using pip install -e folder. :D My first package!!!!

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='nested_forms',
+      version='0.1',
+      description='An example Django app that shows how to do nested forms',
+      url='http://github.com/jaywhy13/nested_forms',
+      author='Jean-Mark Wright',
+      author_email='jeanmark.wright@gmail.com',
+      license='MIT',
+      packages=['nest', 'nested_forms'],
+      install_requires = [
+	      'Django==1.5.5',
+	      'argparse==1.2.1',
+	      'django-crispy-forms==1.4.0',
+	      'django-extensions==1.2.5',
+	      'six==1.4.1',
+	      'wsgiref==0.1.2',
+      ],
+      zip_safe=False)
