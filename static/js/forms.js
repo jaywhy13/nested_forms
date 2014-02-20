262dbc5c1d34f8e3c3cb5c7e6a7a388f843f00b8
Jean-Mark Wright <jeanmark.wright@gmail.com>
2014-02-19 20:29:29 -0500

    Changed the functions here to use a ManagementForm ViewForm thingy for managing the forms. Just started writing the addChildForm, not quite done yet.

--- static/js/forms.js
+++ static/js/forms.js
@@ -1,21 +1,23 @@
 
-function addChildForm(inputDom, form, formPrefix){
-    console.log("Adding form of type: ", form);
-    // Figure out what index this needs to be...
-    // Use the count as the current Id then increment it...
-    var sel = "#id_" + formPrefix + "-TOTAL_FORMS";
-    console.log("Selector: ", sel);
-    var count = parseInt($(sel).val());
-    $(sel).val(count + 1);
 
-    console.log("The index is now: " + count);
+function ManagementForm(parentFormName, childTemplateForm){
+    this.childTemplateForm = childTemplateForm; // BuildingForm-template
+    this.parentFormName = parentFormName;
+    this.childrenDivFormName = this.parentFormName + "_children_div";
 
-    dust.render(form, {"index" : count}, function(err, out){
-        if(err){
-            console.log(err);
-        }
-        var target = $(inputDom).parent();
-        target.append($(out));
-
-    }); 
+    // Declare some observables
+    this.totalForms = ko.observable(0);
+    this.maxForms = ko.observable(0);
+    this.initialForms = ko.observable(0);
 }
+
+ManagementForm.prototype.addChildForm = function() {
+    // Adds a child form to this form
+    var div = jQuery("<div/>");
+    div.attr("data-bind", "template: " + this.childTemplateForm);
+    div.appendTo(jQuery("." + this.childrenDivFormName));
+    //console.log(div.get()[0]);
+    // ko.applyBindings(this, div.get()[0]);
+    // this.totalForms( this.totalForms() + 1 );
+};
+
