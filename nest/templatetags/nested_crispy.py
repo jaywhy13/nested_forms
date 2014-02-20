c6f2b9abd476d62cd955c7d66e9792bf96f25ef5
Jean-Mark Wright <jeanmark.wright@gmail.com>
2014-02-19 20:28:32 -0500

    Changed CompositeNode to NestedFormNode and added additional nodes for KnockoutTemplateNode and HtmlContentNode and KnockoutTemplateJsNode. This seems to be working so far. It will print out the script template for the child form and also has bindings attached for the management form and calls applyBindings on the parent form.

--- nest/templatetags/nested_crispy.py
+++ nest/templatetags/nested_crispy.py
@@ -1,11 +1,85 @@
 from django.template import Node, NodeList
 from django import template
+from django.utils.safestring import mark_safe
+
 from crispy_forms.helper import FormHelper
 from crispy_forms.templatetags.crispy_forms_tags import CrispyFormNode
 
 register = template.Library()
 
-class CompositeNode(Node):
+
+class HtmlContent(Node):
+
+    def __init__(self, html):
+        self.html = html
+
+    def render(self, context):
+        return mark_safe(self.html)
+
+class KnockoutFormTemplate(Node):
+    """ This template node is responsible for rendering <script> templates
+        for all children form of a given form.
+        TODO: Ensure templates are not rendered twice.
+    """
+
+    def __init__(self, form):
+        self.form_var = template.Variable(form)
+
+    def render(self, context):
+        nodelist = NodeList()
+        form = self.form_var.resolve(context)
+        child_forms = []
+        # recursively find all inline_form that need to get printed
+        while hasattr(form, "child_form"):
+            child_forms.append((form.child_form, form))
+            form = form.child_form
+
+        if not child_forms:
+            print("%s has no inline forms" % type(form))
+
+        # Loop through each and get the knockout templates for each
+        for child_form, parent_form in child_forms:
+            form_name = child_form.__name__
+            template_name = "%s-template" % form_name
+            context["child_%s" % form_name] = child_form
+            nodelist.append(HtmlContent('<script type="text/html" id="%s">' % template_name))
+            nodelist.append(CrispyFormNode(form="child_%s" % form_name, helper=None))
+            nodelist.append(HtmlContent("</script>"))
+        return nodelist.render(context)
+
+class NestedFormNodeJs(Node):
+    """ This is node for printing out the JS code to activate knockout for the 
+        management form.
+    """
+    def __init__(self, form):
+        self.form = form
+        self.form_var = template.Variable(form)
+
+    def render(self, context):
+        actual_form = self.form_var.resolve(context)
+        child_template_name = "%s-template" % actual_form.child_form.__name__
+        form_name = actual_form.get_form_name()
+
+        nodelist = NodeList()
+        form_div_class = "%s_form_div" % form_name
+
+        # Add the script to activate this form
+        nodelist.append(HtmlContent("""
+            <script> 
+                $(document).ready(function(){
+                    ko.applyBindings(new ManagementForm('%s', '%s'), $(".%s").get()[0]);
+                });
+            </script>
+            """ % (form_name, child_template_name, form_div_class)))
+        nodelist.append(HtmlContent("</div>"))
+
+        return nodelist.render(context)
+
+
+class NestedFormNode(Node):
+    """ This is the node for the `nested_form` tag. This is responsible for
+        printing out the parent form, any children form, management form (hidden)
+    """
 
     def __init__(self, form, helper):
         self.form = form
@@ -15,6 +89,7 @@ class CompositeNode(Node):
 
     def render(self, context):
         actual_form = self.form_var.resolve(context)
+        form_name = actual_form.get_form_name()
         if self.helper is not None:
             actual_helper = self.helper_var.resolve(context)
         else:
@@ -27,25 +102,59 @@ class CompositeNode(Node):
         management_form_helper = FormHelper()
         management_form_helper.form_tag = False
         management_form_helper.disable_csrf = True
+        management_form = actual_form.inline_form.management_form
 
+        """ Basically we create a node list and all the following things to it:
+            - <div class='FormName_form_div'> - HtmlContent
+                - The actual form (parent)
+                -  The management form (hidden)
+                - Inline actions form (if one exists)
+                - <div class='FormName_children_div'></div>
+                - Knockout form templates
+            - </div>
+            Then we return nodelist.render
+        """
 
         # Add the forms to the nodelist
         nodelist = NodeList()
         # Add the main form to our node list
+
+        # Create a DIV to store the entire form
+        form_div_class = "%s_form_div" % form_name
+        nodelist.append(HtmlContent("<div class='%s'>" % form_div_class))
         nodelist.append(CrispyFormNode(form=self.form, helper=self.helper))
 
         # Stuff some other stuff in the context for resolving later
         context["inline_form"] = actual_form.inline_form
-        context["inline_form_management"] = actual_form.inline_form.management_form
+        context["inline_form_management"] = management_form
         context["management_form_helper"] = management_form_helper
         # Seems we don't need the inline form bcuz the management form prints it out...
         #nodelist.append(CrispyFormNode(form="inline_form", helper=self.helper))
-        nodelist.append(CrispyFormNode(form="inline_form_management", helper="management_form_helper"))
+
+        # Place a data binding on the management form for KnockoutJS
+        fields = management_form.fields
+        fields["TOTAL_FORMS"].widget.attrs["data-bind"] = "value: totalForms"
+        fields["INITIAL_FORMS"].widget.attrs["data-bind"] = "value: initialForms"
+        fields["MAX_NUM_FORMS"].widget.attrs["data-bind"] = "value: maxForms"
+
+        nodelist.append(CrispyFormNode(form="inline_form_management", 
+            helper="management_form_helper"))
+
         # Check if there is an inline form
         if hasattr(actual_form, "inline_actions_form") and \
                 actual_form.inline_actions_form is not None:
             context["inline_actions_form"] = actual_form.inline_actions_form
             nodelist.append(CrispyFormNode(form="inline_actions_form", helper=self.helper))
+
+        nodelist.append(KnockoutFormTemplate(self.form))
+
+        # Add a DIV for the children
+        children_div_class = "%s_children_div" % form_name
+        nodelist.append(HtmlContent("<div class='%s'>" % children_div_class))
+        # Current KIDS should be printed here BTW
+        nodelist.append(HtmlContent("</div>"))
+
+        nodelist.append(HtmlContent("</div>"))
         return nodelist.render(context)
 
 @register.tag
@@ -56,4 +165,10 @@ def nested_form(parser, token):
         helper = tokens.pop(1)
     except IndexError:
         helper = None
-    return CompositeNode(form, helper)
+    return NestedFormNode(form, helper)
+
+@register.tag
+def nested_form_js(parser, token):
+    tokens = token.split_contents()
+    form = tokens.pop(1)
+    return NestedFormNodeJs(form)
