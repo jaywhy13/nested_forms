a5c95f1c7430dd6fa1309aa0d697a189a6bac6b0
Jean-Mark Wright <jeanmark.wright@gmail.com>
2014-02-21 16:23:25 -0500

    Added unicode funcs.

--- nest/models.py
+++ nest/models.py
@@ -4,11 +4,17 @@ from django.db import models
 class Block(models.Model):
 	name = models.CharField("Block Name", max_length=255)
 
+	def __unicode__(self):
+		return "Block: %s" % self.name
+
 
 class Building(models.Model):
 	name = models.CharField("Bldg Name", max_length=255)
 	block = models.ForeignKey("Block", blank=True, null=True, 
 		related_name="buildings")
+
+	def __unicode__(self):
+		return "Building: %s" % self.name
 
 class Tenant(models.Model):
 	first_name = models.CharField(max_length=255)
@@ -16,3 +22,5 @@ class Tenant(models.Model):
 	building = models.ForeignKey("Building", blank=True, null=True, 
 		related_name="tenants")
 
+	def __unicode__(self):
+		return "Tenant: %s" % self.name
