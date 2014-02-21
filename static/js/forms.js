a13b49e66bec02976481ac8c3c546600199683c3
Jean-Mark Wright <jeanmark.wright@gmail.com>
2014-02-21 16:28:17 -0500

    Changed the name of the childTemplateForm variabe... dropped off the "Form" part.

--- static/js/forms.js
+++ static/js/forms.js
@@ -1,8 +1,9 @@
 
 
-function ManagementForm(parentFormName, childTemplateForm, initialForms){
+function ManagementForm(parentFormName, childTemplate, initialForms){
+    
     var self = this;
-    self.childTemplateForm = childTemplateForm; // BuildingForm-template
+    self.childTemplate = childTemplate; // BuildingForm-template
     self.parentFormName = parentFormName;
     self.childrenDivFormName = self.parentFormName + "_children_div";
 
@@ -14,7 +15,7 @@ function ManagementForm(parentFormName, childTemplateForm, initialForms){
     self.addChildForm = function() {
         // Adds a child form to this form
         var div = jQuery("<div/>");
-        div.attr("data-bind", "template: {name: '" + self.childTemplateForm + "', data: {index: " + self.totalForms() + "}}");
+        div.attr("data-bind", "template: {name: '" + self.childTemplate + "', data: {index: " + self.totalForms() + "}}");
         div.appendTo(jQuery("." + self.childrenDivFormName));
         self.totalForms( self.totalForms() + 1 );
         ko.applyBindings(this, div.get()[0]);
