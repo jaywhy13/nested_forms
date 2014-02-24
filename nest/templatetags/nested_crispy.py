17d0d8fd9e811c4a7cc2fbdff5bb06b2c58cf44f
Jean-Mark Wright <jeanmark.wright@gmail.com>
2014-02-24 12:20:37 -0500

    Made some crucial changes here. Added a link from the inline form back to it's form and what we do is that we use the form as the naming for all the different components. Since the formsets don't have a prefix, that was quite useless to us, so we needed to really maintain a linkage back to the actual form that holds the formset. Made the updates the JS and template code so that addition of nested forms works nicely now. CRUD works at the first level, need to dig in to find out why it's not working at the levels below that.

--- nest/templatetags/nested_crispy.py
+++ nest/templatetags/nested_crispy.py
@@ -10,6 +10,35 @@ from nest.forms import NestedModelForm
 
 register = template.Library()
 
+def get_form_template_name(form):
+    if isinstance(form, type):
+        form = form()
+    return "%s-template" % form.__class__.__name__
+
+def get_management_form_div_name(form):
+    return "%s-management-form-div" % get_form_name(form)
+
+def get_form_name(form):
+    """ Use a unified form naming function that names forms and base formsets.
+        Forms are named which include their prefix to avoid clashes with 
+        other forms in the context.
+        Note that we don't have to worry about this since there will only ever
+        be one base formset per level so there's no risk of clashing with 
+        other formsets in the context.
+        The base formset is therefore named simply by using the class name of 
+        the form it holds. 
+    """
+    is_formset = issubclass(form.__class__, BaseFormSet)
+    if not is_formset:
+        if isinstance(form, type): # if it's a class, just return the name
+            return form.__name__
+        form_name = "%s-form-%s" % (form.__class__.__name__, 
+            form.prefix) if form.prefix else \
+            "%s-form" % (form.__class__.__name__)
+    else:
+        form_name = "%s-formset" % form.form.__name__
+    return form_name
+
 def get_default_helper():
     helper = FormHelper()
     helper.form_tag = False
@@ -28,7 +57,6 @@ class KnockoutFormTemplate(Node):
         for all children form of a given form.
         TODO: Ensure templates are not rendered twice.
     """
-
     def __init__(self, form):
         self.form_var = template.Variable(form)
 
@@ -51,8 +79,8 @@ class KnockoutFormTemplate(Node):
             prefix = parent_form.inline_form.prefix
             for field_name, field in child_form.fields.iteritems():
                 field.widget.attrs["data-bind"] = mark_safe("attr: { id: 'id_%s' + '-' + index + '-%s', name: '%s-' + index + '-%s'}" % (prefix, field_name, prefix, field_name))
-            form_name = child_form.__class__.__name__
-            template_name = "%s-template" % form_name
+            form_name = get_form_name(child_form)
+            template_name = get_form_template_name(child_form)
             context["child_%s" % form_name] = child_form
             context["child_%s_helper" % form_name] = get_default_helper()
 
@@ -77,6 +105,34 @@ class KnockoutFormTemplate(Node):
             nodelist.append(HtmlContent("</script>"))
         return nodelist.render(context)
 
+
+def get_bindings(form):
+    if not hasattr(form, "inline_form"):
+        return []
+
+    formset = form.inline_form
+    form_name = get_form_name(form)
+    child_template_name = get_form_template_name(formset.form)
+    parent_form_div_class = "%s_form_div" % form_name
+    num_forms = len(formset.forms)
+    management_form_div_class = get_management_form_div_name(formset.parent_form)
+
+    bindings = []
+
+    bindings.append(
+        """
+            ko.applyBindings(new ManagementForm('%s', '%s', %s), jQuery(".%s").get()[0]);
+        """ % (form_name, child_template_name, num_forms, management_form_div_class)
+    )
+
+    for child_form in formset.forms:
+        child_bindings = get_bindings(child_form)
+        for child_binding in child_bindings:
+            bindings.append(child_binding)
+
+    return bindings
+
+
 class NestedFormNodeJs(Node):
     """ This is node for printing out the JS code to activate knockout for the 
         management form.
@@ -87,23 +143,19 @@ class NestedFormNodeJs(Node):
 
     def render(self, context):
         actual_form = self.form_var.resolve(context)
-        child_template_name = "%s-template" % actual_form.child_form.__name__
-        form_name = actual_form.__class__.__name__
-
-        num_forms = len(actual_form.inline_form.forms)
-
+        bindings = get_bindings(actual_form)
+        #bindings.reverse() # bind in reverse order ...
         nodelist = NodeList()
-        form_div_class = "%s_form_div" % form_name
 
         # Add the script to activate this form
         nodelist.append(HtmlContent("""
             <script> 
                 $(document).ready(function(){
                     // new ManagementForm(parentForm, childTemplate, initialForms);
-                    ko.applyBindings(new ManagementForm('%s', '%s', %s), jQuery(".%s").get()[0]);
+                    %s
                 });
             </script>
-            """ % (form_name, child_template_name, num_forms, form_div_class)))
+            """ % "\n".join(bindings)))
         nodelist.append(HtmlContent("</div>"))
 
         return nodelist.render(context)
@@ -131,9 +183,8 @@ class NestedFormNode(Node):
         top_level = self.top_level
         actual_form = self.form_var.resolve(context)
         is_formset = issubclass(actual_form.__class__, BaseFormSet)
-        form_name = actual_form.__class__.__name__ if not is_formset \
-            else actual_form.form.__name__
-        print("Rendering %s (%s) " % (form_name, is_formset))
+        form_name = get_form_name(actual_form) if not is_formset else get_form_name(actual_form.parent_form)
+        print("== Rendering %s (Formset: %s) " % (form_name, is_formset))
         if self.helper is not None:
             actual_helper = self.helper_var.resolve(context)
         else:
@@ -144,13 +195,17 @@ class NestedFormNode(Node):
             actual_helper.form_tag = False
             #raise template.TemplateSyntaxError("form_tag cannot be True on the helper, please go and rectify that")
 
+        # Place a link on the formset back to its parent form
+        if not is_formset and hasattr(actual_form, "inline_form"):
+            actual_form.inline_form.parent_form = actual_form
+
         """ Basically we create a node list and all the following things to it:
             - <div class='FormName_form_div'> - HtmlContent
                 - The actual form (parent)
                 -  The inline form which prints the children and management form (hidden) 
                 - <div class='FormName_children_div'></div>
                 - Inline actions form (if one exists)
-                - Knockout form templates
+                - <script>Knockout form templates</script>
             - </div>
             Then we return nodelist.render
         """
@@ -160,19 +215,21 @@ class NestedFormNode(Node):
         # Add the main form to our node list
 
         # === PRINT THE PARENT FORM
-        form_div_class = "%s_form_div" % form_name
-        nodelist.append(HtmlContent("<div class='%s'>" % form_div_class))
         if not is_formset:
+            form_div_class = "%s_form_div" % form_name
+            print("Creating form div class: %s" %form_div_class)
+            nodelist.append(HtmlContent("<div class='%s'>" % form_div_class))
             nodelist.append(CrispyFormNode(form=self.form, helper=self.helper))
-        else:
-            print("Prefix: %s, Default prefix: %s" % (actual_form.prefix, actual_form.get_default_prefix()))
 
         # === PRINT THE KIDS
         if is_formset: # e.g. BuildingFormSet
             # We need to add each form AND print the management form
+            # Add a DIV for the children
+            children_div_class = "%s_children_div" % form_name
+            nodelist.append(HtmlContent("<div class='%s'>" % children_div_class))
             for child_index in range(len(actual_form.forms)):
                 child_form = actual_form.forms[child_index]
-                child_form_name = "%s_%s" % (child_form.__class__.__name__, child_index)
+                child_form_name = "%s_%s" % (get_form_name(child_form), child_index)
                 print(" Adding %s to the nodelist" % child_form_name)
                 child_form_helper_name = "%s_helper" % child_form_name
                 context[child_form_name] = child_form
@@ -184,8 +241,9 @@ class NestedFormNode(Node):
                     print(" %s (%s) is NOT a NestedModelForm, wrapping it in a crispy node" % (child_form_name, child_form.__class__))
                     nodelist.append(CrispyFormNode(child_form_name, 
                         child_form_helper_name if hasattr(child_form, "helper") else None))
+            nodelist.append(HtmlContent("</div>"))
 
-            # We need to print the management form
+            # We need to print the management form for these kids
             management_form_helper = FormHelper()
             management_form_helper.form_tag = False
             management_form_helper.disable_csrf = True
@@ -201,9 +259,24 @@ class NestedFormNode(Node):
 
             # Let Crispy handle the printing of this...
             print("Adding %s management_form to the node list" % form_name)
+            # Add a div for the management form
+            management_form_div_class = get_management_form_div_name(actual_form.parent_form)
+            nodelist.append(HtmlContent("<div class='%s'>" % management_form_div_class))
             nodelist.append(CrispyFormNode("%s_management_form" % form_name,
                 "%s_management_form_helper" % form_name))
-        else:
+
+            # Check if there is an inline form
+            if hasattr(actual_form, "actions_form") and \
+                    actual_form.actions_form is not None:
+                inline_actions_form_name = "%s-inline_actions_form" % get_form_name(actual_form.actions_form)
+                inline_actions_form_helper_name = "%s-helper" % inline_actions_form_name
+                context[inline_actions_form_name] = actual_form.actions_form
+                context[inline_actions_form_helper_name] = actual_form.actions_form().helper
+                nodelist.append(CrispyFormNode(form=inline_actions_form_name, 
+                    helper=inline_actions_form_helper_name))
+            nodelist.append(HtmlContent("</div>"))
+
+        else: # not a formset
             """ We have two cases to deal with here:
                 - BlockForm - it has an inline form that we need to print here
                 - TenantForm - no inline form to worry about...
@@ -214,20 +287,11 @@ class NestedFormNode(Node):
                 context["%s_inline_form" % form_name] = actual_form.inline_form
                 nodelist.append(NestedFormNode("%s_inline_form" % form_name, self.helper, top_level=False))
 
-        # Check if there is an inline form
-        if hasattr(actual_form, "inline_actions_form") and \
-                actual_form.inline_actions_form is not None:
-            context["inline_actions_form"] = actual_form.inline_actions_form
-            nodelist.append(CrispyFormNode(form="inline_actions_form", helper=self.helper))
 
 
-        # Add a DIV for the children
-        children_div_class = "%s_children_div" % form_name
-        nodelist.append(HtmlContent("<div class='%s'>" % children_div_class))
-        # Current KIDS should be printed here BTW
-        nodelist.append(HtmlContent("</div>"))
-
-        nodelist.append(HtmlContent("</div>"))
+        if not is_formset:
+            # we didn't add this if it was a formset
+            nodelist.append(HtmlContent("</div>")) # for the form
 
         if top_level:
             # print out ALL the script templates
