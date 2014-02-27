c3e843e6d1da67ed0c9c365abec7bb24baffda7f
Jean-Mark Wright <jeanmark.wright@gmail.com>
2014-02-26 22:38:16 -0500

    Allllllmost there... right now after we add a building, the tenants are being added to the wrong building. Something is off with the prefixes.

--- nest/templatetags/nested_crispy.py
+++ nest/templatetags/nested_crispy.py
@@ -12,15 +12,31 @@ from nest.forms import NestedModelForm
 
 register = template.Library()
 
+""" 
+    All the nested forms we print out will have this general structure
+    - <div id='{form_name}_form_div' class='form-container'> - HtmlContent
+        - Fields from the parent form 
+        -  The inline form which prints the children and management form (hidden) 
+        - <div id='{form_name}_children_div' class='form-children'>
+            --- Kids printed here ---
+          </div>
+        - <div id='{form_name}_management_form_div' class='management-form-div'>
+            - Management form (hidden)
+            - Inline actions form (if one exists)
+        - </div>
+    - </div>
+"""
+
+
 def get_form_template_name(form):
     if isinstance(form, type):
         form = form()
     return "%s-template" % form.__class__.__name__
 
-def get_management_form_div_name(form):
-    return "%s-management-form-div" % get_form_name(form)
+def get_management_form_div_name(form, prefix=""):
+    return "%s_management_form_div" % get_form_name(form, prefix=prefix)
 
-def get_form_name(form):
+def get_form_name(form, prefix=""):
     """ Use a unified form naming function that names forms and base formsets.
         Forms are named which include their prefix to avoid clashes with 
         other forms in the context.
@@ -30,13 +46,15 @@ def get_form_name(form):
         The base formset is therefore named simply by using the class name of 
         the form it holds. 
     """
+
     is_formset = issubclass(form.__class__, BaseFormSet)
     if not is_formset:
         if isinstance(form, type): # if it's a class, just return the name
             return form.__name__
-        form_name = "%s-form-%s" % (form.__class__.__name__, 
-            form.prefix) if form.prefix else \
-            "%s-form" % (form.__class__.__name__)
+        if form.prefix:
+            prefix = form.prefix
+        form_name = "%s-form-%s" % (form.__class__.__name__, prefix) if prefix \
+            else "%s-form" % (form.__class__.__name__)
     else:
         form_name = "%s-formset" % form.form.__name__
     return form_name
@@ -75,6 +93,21 @@ class KnockoutFormTemplate(Node):
             child_forms.append((form.child_form, form))
             form = form.child_form()
 
+        """ 
+            All the nested forms we print out will have this general structure
+            - <div id='{form_name}_form_div' class='form-container'> - HtmlContent
+                - Fields from the parent form 
+                -  The inline form which prints the children and management form (hidden) 
+                - <div id='{form_name}_children_div' class='form-children'>
+                    --- Kids printed here ---
+                  </div>
+                - <div id='{form_name}_management_form_div' class='management-form-div'>
+                    - Management form (hidden)
+                    - Inline actions form (if one exists)
+                - </div>
+            - </div>
+        """
+
         # Loop through each and get the knockout templates for each
         for child_form, parent_form in child_forms:
             child_form = child_form() if isinstance(child_form, type) else child_form
@@ -87,18 +120,32 @@ class KnockoutFormTemplate(Node):
             context["child_%s" % form_name] = child_form
             context["child_%s_helper" % form_name] = get_default_helper()
 
+            # print out the script for the template
             nodelist.append(HtmlContent('<script type="text/html" id="%s">' % template_name))
+            form_container_attrs = "{'id': '%s_form_div' }" % get_form_name(child_form, prefix="' + prefix + '-' + index + '")
+            nodelist.append(HtmlContent('<div data-bind="attr: %s" class="form-container">' % mark_safe(form_container_attrs)))
             nodelist.append(CrispyFormNode(form="child_%s" % form_name, 
                 helper="child_%s_helper" % form_name))
             if hasattr(child_form, "inline_form"): # then print the management form as well
                 """ If our child has a child (e.g. building has tenants)
                     then print the management form for it's child (tenant)
                 """
-                management_form_div_class = get_management_form_div_name(parent_form)
-                nodelist.append(HtmlContent("<div class='%s'>" % management_form_div_class))
+                # first we need to actually print the div to hold the kids
+                form_children_attrs = "{'id': '%s_children_div' }" % \
+                    get_form_name(child_form, prefix="' + prefix + '-' + index +'")        
+                nodelist.append(HtmlContent("""
+                    <div data-bind="attr: %s" class="form-children"></div>
+                """ % mark_safe(form_children_attrs)))
+
+                management_form_div_class = get_management_form_div_name(parent_form, prefix="' + prefix + '-' + index + '")
+                management_form_div_attrs = "{'id' : '%s'}" % management_form_div_class
+                nodelist.append(HtmlContent("""
+                    <div data-bind="attr: %s" class='management-form-div'>
+                    """ % management_form_div_attrs))
 
                 formset = child_form.inline_form
                 grand_child_management_form = child_form.inline_form.management_form
+
                 # Tweak it and adds Knockout bindings 
                 parent_prefix = parent_form.inline_form.prefix
                 child_prefix = child_form.inline_form.prefix
@@ -117,11 +164,11 @@ class KnockoutFormTemplate(Node):
                     inline_actions_form_helper_name = "%s-helper" % inline_actions_form_name
                     context[inline_actions_form_name] = formset.actions_form
                     context[inline_actions_form_helper_name] = formset.actions_form().helper
-                    #nodelist.append(CrispyFormNode(form=inline_actions_form_name, 
-                    #    helper=inline_actions_form_helper_name))
+                    nodelist.append(CrispyFormNode(form=inline_actions_form_name, 
+                        helper=inline_actions_form_helper_name))
                 
-                nodelist.append(HtmlContent("<div/>"))
-
+                nodelist.append(HtmlContent("</div>")) # end of the management form div
+            nodelist.append(HtmlContent("</div>")) # end of form-cotnainer
 
             nodelist.append(HtmlContent("</script>"))
         return nodelist.render(context)
@@ -143,8 +190,9 @@ def get_bindings(form):
 
     bindings.append(
         """
-            ko.applyBindings(new ManagementForm('%s', '%s', %s, '%s'), jQuery(".%s").get()[0]);
-        """ % (form_name, child_template_name, num_forms, prefix, management_form_div_class)
+            // Management form for the children of %s
+            ko.applyBindings(new ManagementForm('%s', '%s', %s, '%s'), jQuery("#%s").get()[0]);
+        """ % (form_name, form_name, child_template_name, num_forms, prefix, management_form_div_class)
     )
 
     for child_form in formset.forms:
@@ -153,6 +201,23 @@ def get_bindings(form):
             bindings.append(child_binding)
 
     return bindings
+
+
+def get_js_info(form):
+    """ Returns a list of assignments that indicate the name of the 
+        child template and child form name given a form name
+    """
+    infos = {}
+    while hasattr(form, "child_form"):
+        info = dict(childTemplate="%s-template" % get_form_name(form.child_form),
+            childForm="%s" % get_form_name(form.child_form),
+            relName=form.inline_form.rel_name
+            )
+        infos[get_form_name(form.__class__)] = info
+        form = form.child_form()
+    return """
+        var childInfos = %s;
+    """ % json.dumps(infos)
 
 
 class NestedFormNodeJs(Node):
@@ -169,15 +234,19 @@ class NestedFormNodeJs(Node):
         #bindings.reverse() # bind in reverse order ...
         nodelist = NodeList()
 
+        js_info = get_js_info(actual_form)
+
         # Add the script to activate this form
         nodelist.append(HtmlContent("""
             <script> 
+                %s 
+
                 $(document).ready(function(){
                     // new ManagementForm(parentForm, childTemplate, initialForms);
                     %s
                 });
             </script>
-            """ % "\n".join(bindings)))
+            """ % (js_info, "\n".join(bindings))))
         nodelist.append(HtmlContent("</div>"))
 
         return nodelist.render(context)
@@ -222,12 +291,16 @@ class NestedFormNode(Node):
             actual_form.inline_form.parent_form = actual_form
 
         """ Basically we create a node list and all the following things to it:
-            - <div class='FormName_form_div'> - HtmlContent
-                - The actual form (parent)
+            - <div id='{form_name}_form_div' class='form-container'> - HtmlContent
+                - Fields from the parent form 
                 -  The inline form which prints the children and management form (hidden) 
-                - <div class='FormName_children_div'></div>
-                - Inline actions form (if one exists)
-                - <script>Knockout form templates</script>
+                - <div id='{form_name}_children_div' class='form-children'>
+                    --- Kids printed here ---
+                  </div>
+                - <div id='{form_name}_management_form_div' class='management-form-div'>
+                    - Management form (hidden)
+                    - Inline actions form (if one exists)
+                - </div>
             - </div>
             Then we return nodelist.render
         """
@@ -240,7 +313,7 @@ class NestedFormNode(Node):
         if not is_formset:
             form_div_class = "%s_form_div" % form_name
             #print("Creating form div class: %s" %form_div_class)
-            nodelist.append(HtmlContent("<div class='%s form-container'>" % form_div_class))
+            nodelist.append(HtmlContent("<div id='%s' class='form-container'>" % form_div_class))
             nodelist.append(CrispyFormNode(form=self.form, helper=self.helper))
 
         # === PRINT THE KIDS
@@ -248,7 +321,7 @@ class NestedFormNode(Node):
             # We need to add each form AND print the management form
             # Add a DIV for the children
             children_div_class = "%s_children_div" % form_name
-            nodelist.append(HtmlContent("<div class='%s'>" % children_div_class))
+            nodelist.append(HtmlContent("<div id='%s' class='form-children'>" % children_div_class))
             for child_index in range(len(actual_form.forms)):
                 child_form = actual_form.forms[child_index]
                 if not hasattr(child_form, "helper"):
@@ -286,7 +359,7 @@ class NestedFormNode(Node):
             #print("Adding %s management_form to the node list" % form_name)
             # Add a div for the management form
             management_form_div_class = get_management_form_div_name(actual_form.parent_form)
-            nodelist.append(HtmlContent("<div class='%s'>" % management_form_div_class))
+            nodelist.append(HtmlContent("<div id='%s' class='management-form-div'>" % management_form_div_class))
             nodelist.append(CrispyFormNode("%s_management_form" % form_name,
                 "%s_management_form_helper" % form_name))
 
--- static/js/forms.js
+++ static/js/forms.js
@@ -1,13 +1,48 @@
 
-
-function ManagementForm(parentFormName, childTemplate, initialForms, prefix){
-
+/**
+ * The Management Form manages bindings for Django formset management forms.
+ * The Django management form stores the following information:
+ * - maximum number of forms addable 
+ * - total of forms currently added (TOTAL_FORMS)
+ * - total number of initial forms 
+ * So basically, we want to use KnockoutJS to update the TOTAL_FORMS variable.
+ * This scope provides an "addChild" function that can be called within the
+ * scope of a management form binding to add a child form. 
+ * In order to achieve this we take as input, the parentFormName, childTemplate 
+ * name, the number of initial forms and also the prefix. 
+ *
+ * Now, every time we add a new management form, we actually need to call 
+ * applyBindings on the entire structure that is being added because we actually
+ * need KnockoutJS to translate some of the bidnings into actual properties. 
+ *
+ * We need to do this because the templates can't store the 'prefix' of the 
+ * element before hand. So the field for TOTAL_FORMS could be called
+ * buildings-0-TOTAL_FORMS or buildings-1-TOTAL_FORMS depending on which child 
+ * it is and so on. So we have to apply bindings to get those variables put in.
+ *
+ * This works fine when the page is loading. We just apply the bindings to the 
+ * management form div container which also has the actions form. We needed to 
+ * put the actions form in that div because more than likely the actions will
+ * need to call addChild or access something else in that scope. 
+ *
+ * Now we need to add a child....
+ * In order to add a child, 
+ * 
+ * 
+ * @param {[type]} parentFormName [description]
+ * @param {[type]} childTemplate  [description]
+ * @param {[type]} initialForms   [description]
+ * @param {[type]} prefix         [description]
+ */
+function ManagementForm(parentFormName, childTemplate, initialForms, childPrefix){
+    // e.g. BlockForm-form, BuildinForm-template, x, buildings
+    // e.g. BuildingForm-form-buildings-0, TenantForm-template, x, buildings-0-tenants
     
     var self = this;
     self.childTemplate = childTemplate; // BuildingForm-template
     self.parentFormName = parentFormName;
     self.childrenDivFormName = self.parentFormName + "_children_div";
-    self.prefix = prefix;
+    self.childPrefix = childPrefix;
 
     // Declare some observables
     self.totalForms = ko.observable(initialForms);
@@ -15,22 +50,59 @@ function ManagementForm(parentFormName, childTemplate, initialForms, prefix){
     self.initialForms = ko.observable(initialForms);
 
     self.addChildForm = function() {
-        console.log("Adding child " + this.childTemplate + " to " + this.childrenDivFormName);
+        console.log("Adding child " + this.childTemplate + " to " + this.childrenDivFormName + " with childPrefix: " + this.childPrefix, " parent is: ", this.parentFormName);
+
         // Adds a child form to this form
         var div = jQuery("<div/>");
-        var templateData = {
-            name : self.childTemplate,
-            data : {
-                index: self.totalForms(),
-                prefix : self.prefix
-            }
-        };
-        div.attr("data-bind", "template: " + JSON.stringify(templateData));
-        div.appendTo(jQuery("." + self.childrenDivFormName));
-        if($("." + self.childrenDivFormName).length == 0){
+        div.attr("data-bind", "template: {name: '" + self.childTemplate + "'}");
+        div.addClass("child-form-wrapper");
+        div.appendTo(jQuery("#" + self.childrenDivFormName));
+        if($("#" + self.childrenDivFormName).length == 0){
             console.log("Warning: " + this.childrenDivFormName + " container does not exist");
         }
+
+        console.log(div.get()[0]);
         self.totalForms( self.totalForms() + 1 );
-        ko.applyBindings(this, div.get()[0]);
+        // Setup bindings for the grand child management form
+        var formName = this.parentFormName.split("-")[0];
+        var childForm = getChildFormName(formName);
+        var childFormName = childForm + "-form-" + self.childPrefix + "-0";
+        var grandChildTemplate = getChildTemplateName(childForm);
+        var grandChildManagementFormDivId = childFormName + "_management_form_div";
+        var grandChildPrefix = getChildRelName(childForm);
+
+        var grandChildManagementForm = new ManagementForm(childFormName, 
+            grandChildTemplate, 0, grandChildPrefix);
+        grandChildManagementForm.index = self.totalForms();
+        grandChildManagementForm.prefix = self.childPrefix;
+
+        ko.applyBindings(grandChildManagementForm, div.get()[0]);
+        //console.log("Data for: ", ko.dataFor(div.get()[0]));
+        //console.log("Context for: ", ko.dataFor(div.get()[0]));
+
     };
 }
+
+/**
+ * Returns the name of the child template given the name of the form class (parent)
+ * @param  {String} parentForm 
+ * @return {String} - the name of the template
+ */
+function getChildTemplateName(parentForm){
+    return childInfos[parentForm]["childTemplate"];
+}
+
+/**
+ * Returns the name of the child form class
+ * @param  {String} parentForm 
+ * @return {String} - the name of the child form
+ */
+function getChildFormName(parentForm){
+    return childInfos[parentForm]["childForm"];
+}
+
+function getChildRelName(parentForm){
+    return childInfos[parentForm]["relName"];
+}
+
+
