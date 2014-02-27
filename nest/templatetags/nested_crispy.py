d8f83b8f5f1d8598f58e5c4ffb4bc022fe512759
Jean-Mark Wright <jeanmark.wright@gmail.com>
2014-02-27 09:49:33 -0500

    Turned out we forgot to attach the binding for the management form, so dynamic additions of a complete tree are now saved. YESSSSSSSS!!!!!!!!!!!! Now to deal with truncating data when there is a whole subtree that is affected.

--- nest/templatetags/nested_crispy.py
+++ nest/templatetags/nested_crispy.py
@@ -152,6 +152,8 @@ class KnockoutFormTemplate(Node):
                 fields = grand_child_management_form.fields
                 for field_name, field in fields.iteritems():
                     attr = "{'id' : 'id_' + prefix + '-' + index + '-%s-%s', 'name' : prefix + '-' + index + '-%s-%s'}" % (child_prefix, field_name, child_prefix, field_name)
+                    if field_name == "TOTAL_FORMS":
+                        attr = "{'id' : 'id_' + prefix + '-' + index + '-%s-%s', 'name' : prefix + '-' + index + '-%s-%s', 'value': totalForms }" % (child_prefix, field_name, child_prefix, field_name)
                     field.widget.attrs["data-bind"] = mark_safe("attr: %s" % attr)
                 context["child_%s_management_form" % form_name] = grand_child_management_form
                 context["child_%s_management_form_helper" % form_name] = get_default_helper()
